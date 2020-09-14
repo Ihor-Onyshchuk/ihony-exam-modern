0f2caa06d64fc44daee4eb9315bcbc0069af9cfe
Ігор Онищук <onyshchukihor11@gmail.com>
2020-09-14 09:40:28 +0300

    added create form

--- src/webparts/ihonyModern/components/IhonyModern.tsx
+++ src/webparts/ihonyModern/components/IhonyModern.tsx
@@ -12,11 +12,25 @@ interface IProps {
 }
 
 interface IState {
-  goods: any[]
+  title: string;
+  name: string;
+  category: string;
+  company: string;
+  country: string;
+  description: string;
+  inStock: string;
+  goods: any[];
 }
 
 export default class IhonyModern extends React.Component<IProps, IState> {
   state={
+    title: '',
+    name: '',
+    company: '',
+    country: '',
+    description: '',
+    category: '',
+    inStock: 'Yes',
     goods: null,
   }
 
@@ -31,14 +45,26 @@ export default class IhonyModern extends React.Component<IProps, IState> {
       .get();
 
     this.setState({goods})
-    console.log(goods)
+  }
+
+  public handleInputChange = (event: any) => {
+    const value = event.target.value;
+    const name = event.target.type === 'select-one'
+      ? 'category'
+      : event.target.name
+
+    this.setState({[name]: value} as React.ComponentState);
+  }
+
+  public handleSubmit = (event: any) => {
+    event.preventDefault();
   }
 
   public render(): React.ReactElement<IProps> {
-    const {goods} = this.state;
+    const {title, name, category, company, country, description, inStock, goods} = this.state;
 
     return (
-      <div>
+      <div className="container">
         <h1>Goods List</h1>
         <ul>
           {goods && goods.map(({Title, category, company}) => (
@@ -55,6 +81,89 @@ export default class IhonyModern extends React.Component<IProps, IState> {
             </li>
           ))}
         </ul>
+        <form
+          onChange={this.handleInputChange}
+          onSubmit={this.handleSubmit} 
+        >
+          <div>
+            <div>Title</div>
+            <input
+              type="text" 
+              name="title"
+              value={title}
+            />
+          </div>
+          <div>
+            <div>Name</div>
+            <input
+              type="text" 
+              name="name"
+              value={name}
+            />
+          </div>
+          <div>
+            <div>Company</div>
+            <input
+              type="text" 
+              name="company"
+              value={company}
+            />
+          </div>
+          <div>
+            <div>Country</div>
+            <input
+              type="text" 
+              name="country"
+              value={country}
+            />
+          </div>
+          <div>
+            <div>Category</div>
+            <select
+              value={category}
+            >
+              <option value="food">food</option>
+              <option value="clothes">clothes</option>
+              <option value="medicine">medicine</option>
+            </select>
+          </div>
+          <div>
+            <div>Description</div>
+            <textarea
+              name="description"
+              value={description} 
+              placeholder="Type description"
+            />
+          </div>
+          <div>
+            <div>In stock</div>
+          <div>
+            <label>
+              <input
+                type="radio" 
+                value="Yes"
+                name="inStock"
+                checked={inStock === 'Yes'}
+              />
+              Yes
+            </label>
+          </div>
+          <div>
+            <label>
+              <input
+                type="radio" 
+                value="No"
+                name="inStock"
+                checked={inStock === 'No'}
+              />
+              No
+            </label>
+        </div>
+          </div>
+          <button>
+            Add
+          </button>
+        </form>
       </div>
     );
   }
